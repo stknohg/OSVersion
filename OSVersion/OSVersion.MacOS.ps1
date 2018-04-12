ec43dce2e846c4688186fc9db4bd7af3f864b7e7
stknohg <stknohg@gmail.com>
2018-04-12 19:27:52 +0900

    Update MacOS versioning.

--- OSVersion/OSVersion.MacOS.ps1
+++ OSVersion/OSVersion.MacOS.ps1
@@ -5,11 +5,40 @@ function GetMacVersion () {
     $osDistro = [OSVersion.Distributions]::MacOS
     $majorVer, $minorVer, $buildVer = 0, 0, 0
     switch ([System.Environment]::OSVersion.Version.Major) {
-        15 { $majorVer, $minorVer, $buildVer = 10, 11, [System.Environment]::OSVersion.Version.Minor; continue; } # OSX El Capitan (10.11)
-        16 { $majorVer, $minorVer, $buildVer = 10, 12, [System.Environment]::OSVersion.Version.Minor; continue; } # macOS Sierra (10.12)
-        17 { $majorVer, $minorVer, $buildVer = 10, 13, [System.Environment]::OSVersion.Version.Minor; continue; } # macOS High Sierra (10.13)
+        17 {
+            # macOS High Sierra (10.13)
+            $majorVer, $minorVer = 10, 13
+            $buildVer = switch ([System.Environment]::OSVersion.Version.Minor) {
+                0 { 0; continue; }
+                Default { $_ - 1; continue; }
+            }
+            continue
+        }
+        16 { 
+            # macOS Sierra (10.12)
+            $majorVer, $minorVer = 10, 12
+            $buildVer = switch ([System.Environment]::OSVersion.Version.Minor) {
+                0 { 0; continue; }
+                1 { 1; continue; }
+                Default { $_ - 1; continue; }
+            }
+            continue
+        }
+        15 {
+            # OSX El Capitan (10.11)
+            $majorVer, $minorVer = 10, 11
+            $buildVer = switch ([System.Environment]::OSVersion.Version.Minor) {
+                0 {
+                    if ([string]::IsNullOrEmpty((sw_vers -buildVersion))) { 0 } else{ 1 }
+                    continue
+                }
+                Default { $_; continue; }
+            }
+            continue
+        }
         Default {
-            $majorVer, $minorVer, $buildVer = 10, [System.Environment]::OSVersion.Version.Major - 4, [System.Environment]::OSVersion.Version.Minor; continue;
+            $majorVer, $minorVer, $buildVer = 10, [System.Environment]::OSVersion.Version.Major - 4, [System.Environment]::OSVersion.Version.Minor
+            continue
         }
     }
     $osVersion = New-Object 'System.Version' -ArgumentList @($majorVer, $minorVer, $buildVer)
