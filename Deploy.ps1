d33824161b364ed3752875370577385c7e4addc9
stknohg <stknohg@gmail.com>
2018-04-15 15:08:31 +0900

    Added files for Appveyor.

---
+++ Deploy.ps1
@@ -0,0 +1,23 @@
+$RootPath = Join-Path $PSScriptRoot 'OSVersion'
+
+# Check whether tag release.
+if ($Env:APPVEYOR_REPO_TAG -eq 'false') {
+    Write-Host 'This is not tag release. Skip the deploy script.'
+    exit
+}
+
+# Validate module manifest
+Write-Host 'Validate module manifest...' -ForegroundColor Green
+Test-ModuleManifest -Path (Join-Path $RootPath 'OSVersion.psd1') | Format-List
+if (-not $?) {
+    Write-Error 'Module manifest validation was failed.'
+    exit 
+}
+
+# Publish
+Write-Host 'Publish module...' -ForegroundColor Green
+if ($null -eq $env:NUGET_API_KEY) {
+    Write-Error 'NUGET_API_KEY environment variable not found.'
+    exit 
+}
+Publish-Module -Path $RootPath -NugetApiKey $env:NUGET_API_KEY
